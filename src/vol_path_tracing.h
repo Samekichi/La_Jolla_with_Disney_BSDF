14520441609478262a30360fbcbadf26003e7add
Samekichi <karthus20@icloud.com>
2025-02-04 23:38:48 -0800

    Complete `vol_path_tracing_1. Add image.

--- src/vol_path_tracing.h
+++ src/vol_path_tracing.h
@@ -7,7 +7,31 @@ Spectrum vol_path_tracing_1(const Scene &scene,
                             int x, int y, /* pixel coordinates */
                             pcg32_state &rng) {
     // Homework 2: implememt this!
-    return make_zero_spectrum();
+    /* From path_tracing.h: path_tracing()*/
+    int w = scene.camera.width, h = scene.camera.height;
+    Vector2 screen_pos((x + next_pcg32_real<Real>(rng)) / w,
+        (y + next_pcg32_real<Real>(rng)) / h);
+    Ray camera_ray = sample_primary(scene.camera, screen_pos);
+    // Ray differential for volumetric scattering is an unsolved problem,
+    // so we disable it for volumetric path tracing for now.
+    RayDifferential ray_diff = RayDifferential{ Real(0), Real(0) };
+
+    std::optional<PathVertex> vertex_ = intersect(scene, camera_ray, ray_diff);
+    // For HW 2's volumetric path tracing, we assume there is no environment map in the scene.
+    if (!vertex_) {
+        return make_zero_spectrum();
+    }
+    PathVertex vertex = *vertex_;
+    // Fetch related volumetric parameters at the intersection point
+    Spectrum sigma_a = get_sigma_a(scene.media[scene.camera.medium_id], vertex.position);
+    Real t = distance(camera_ray.org, vertex.position);
+    // Compute the transmittance and emission at the intersection point
+    Spectrum transmittance = exp(-sigma_a * t);
+    Spectrum Le = make_zero_spectrum();
+    if (is_light(scene.shapes[vertex.shape_id])) {
+        Le = emission(vertex, -camera_ray.dir, scene);
+    }
+    return transmittance * Le;
 }
 
 // The second simplest volumetric renderer: 
