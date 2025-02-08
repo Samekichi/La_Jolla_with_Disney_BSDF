ecf7ec63a108e8e84c8cbe750a2280447b9636de
Samekichi <karthus20@icloud.com>
2025-02-08 10:59:31 -0800

    Adjust `render` for partial img at 4 corners.

--- src/render.cpp
+++ src/render.cpp
@@ -8,8 +8,249 @@
 #include "progress_reporter.h"
 #include "scene.h"
 
+#define FULL_IMAGE
+//#define TOP_LEFT  // only render top-left corner 1/4 of the image, for efficient debugging
+//#define TOP_RIGHT
+//#define BOTTOM_LEFT
+//#define BOTTOM_RIGHT
+
+#ifndef RENDER_PERCENT_WIDTH
+#define RENDER_PERCENT_WIDTH 0.33
+#endif
+
+#ifndef RENDER_PERCENT_HEIGHT
+#define RENDER_PERCENT_HEIGHT 0.33
+#endif
+
+static void compute_offsets(int full_w, int full_h, int render_w, int render_h,
+    int& offset_x, int& offset_y) {
+#ifdef FULL_IMAGE
+    offset_x = 0;
+    offset_y = 0;
+#elif defined(TOP_LEFT)
+    offset_x = 0;
+    offset_y = 0;
+#elif defined(TOP_RIGHT)
+    offset_x = full_w - render_w;
+    offset_y = 0;
+#elif defined(BOTTOM_LEFT)
+    offset_x = 0;
+    offset_y = full_h - render_h;
+#elif defined(BOTTOM_RIGHT)
+    offset_x = full_w - render_w;
+    offset_y = full_h - render_h;
+#else
+    // detault to full image
+    offset_x = 0;
+    offset_y = 0;
+#endif
+}
+
+
+#ifndef FULL_IMAGE
+
+
+Image3 aux_render(const Scene& scene) {
+    int full_w = scene.camera.width, full_h = scene.camera.height;
+    // compute render size in pixels
+    int render_w = int(full_w * RENDER_PERCENT_WIDTH);
+    int render_h = int(full_h * RENDER_PERCENT_HEIGHT);
+    int offset_x, offset_y;
+    compute_offsets(full_w, full_h, render_w, render_h, offset_x, offset_y);
+
+    Image3 img(render_w, render_h);
+
+    constexpr int tile_size = 16;
+    int num_tiles_x = (render_w + tile_size - 1) / tile_size;
+    int num_tiles_y = (render_h + tile_size - 1) / tile_size;
+
+    parallel_for([&](const Vector2i& tile) {
+        int x0 = tile[0] * tile_size;
+        int x1 = std::min(x0 + tile_size, render_w);
+        int y0 = tile[1] * tile_size;
+        int y1 = std::min(y0 + tile_size, render_h);
+        for (int y = y0; y < y1; y++) {
+            for (int x = x0; x < x1; x++) {
+                // global coordinates
+                int sample_x = x + offset_x;
+                int sample_y = y + offset_y;
+                Ray ray = sample_primary(scene.camera,
+                    Vector2((sample_x + Real(0.5)) / full_w,
+                        (sample_y + Real(0.5)) / full_h));
+                RayDifferential ray_diff = init_ray_differential(full_w, full_h);
+                if (std::optional<PathVertex> vertex = intersect(scene, ray, ray_diff)) {
+                    Real dist = distance(vertex->position, ray.org);
+                    Vector3 color{ 0, 0, 0 };
+                    if (scene.options.integrator == Integrator::Depth) {
+                        color = Vector3{ dist, dist, dist };
+                    }
+                    else if (scene.options.integrator == Integrator::ShadingNormal) {
+                        color = vertex->shading_frame.n;
+                    }
+                    else if (scene.options.integrator == Integrator::MeanCurvature) {
+                        Real kappa = vertex->mean_curvature;
+                        color = Vector3{ kappa, kappa, kappa };
+                    }
+                    else if (scene.options.integrator == Integrator::RayDifferential) {
+                        color = Vector3{ ray_diff.radius, ray_diff.spread, Real(0) };
+                    }
+                    else if (scene.options.integrator == Integrator::MipmapLevel) {
+                        const Material& mat = scene.materials[vertex->material_id];
+                        const TextureSpectrum& texture = get_texture(mat);
+                        auto* t = std::get_if<ImageTexture<Spectrum>>(&texture);
+                        if (t != nullptr) {
+                            const Mipmap3& mipmap = get_img3(scene.texture_pool, t->texture_id);
+                            Vector2 uv{ modulo(vertex->uv[0] * t->uscale, Real(1)),
+                                       modulo(vertex->uv[1] * t->vscale, Real(1)) };
+                            Real footprint = vertex->uv_screen_size;
+                            Real scaled_footprint = std::max(get_width(mipmap), get_height(mipmap)) *
+                                std::max(t->uscale, t->vscale) * footprint;
+                            Real level = log2(std::max(scaled_footprint, Real(1e-8)));
+                            color = Vector3{ level, level, level };
+                        }
+                    }
+                    img(x, y) = color;
+                }
+                else {
+                    img(x, y) = Vector3{ 0, 0, 0 };
+                }
+            }
+        }
+        }, Vector2i(num_tiles_x, num_tiles_y));
+
+    return img;
+}
+
+
+Image3 path_render(const Scene& scene) {
+    int full_w = scene.camera.width, full_h = scene.camera.height;
+    int render_w = int(full_w * RENDER_PERCENT_WIDTH);
+    int render_h = int(full_h * RENDER_PERCENT_HEIGHT);
+    int offset_x, offset_y;
+    compute_offsets(full_w, full_h, render_w, render_h, offset_x, offset_y);
+
+    Image3 img(render_w, render_h);
+
+    constexpr int tile_size = 16;
+    int num_tiles_x = (render_w + tile_size - 1) / tile_size;
+    int num_tiles_y = (render_h + tile_size - 1) / tile_size;
+
+    ProgressReporter reporter(num_tiles_x * num_tiles_y);
+    parallel_for([&](const Vector2i& tile) {
+        pcg32_state rng = init_pcg32(tile[1] * num_tiles_x + tile[0]);
+        int x0 = tile[0] * tile_size;
+        int x1 = std::min(x0 + tile_size, render_w);
+        int y0 = tile[1] * tile_size;
+        int y1 = std::min(y0 + tile_size, render_h);
+        for (int y = y0; y < y1; y++) {
+            for (int x = x0; x < x1; x++) {
+                // global coordinates
+                int sample_x = x + offset_x;
+                int sample_y = y + offset_y;
+                Spectrum radiance = make_zero_spectrum();
+                int spp = scene.options.samples_per_pixel;
+                for (int s = 0; s < spp; s++) {
+                    radiance += path_tracing(scene, sample_x, sample_y, rng);
+                }
+                img(x, y) = radiance / Real(spp);
+            }
+        }
+        reporter.update(1);
+        }, Vector2i(num_tiles_x, num_tiles_y));
+    reporter.done();
+    return img;
+}
+
+
+Image3 vol_path_render(const Scene& scene) {
+    int full_w = scene.camera.width, full_h = scene.camera.height;
+    int render_w = int(full_w * RENDER_PERCENT_WIDTH);
+    int render_h = int(full_h * RENDER_PERCENT_HEIGHT);
+    int offset_x, offset_y;
+    compute_offsets(full_w, full_h, render_w, render_h, offset_x, offset_y);
+
+    Image3 img(render_w, render_h);
+
+    constexpr int tile_size = 16;
+    int num_tiles_x = (render_w + tile_size - 1) / tile_size;
+    int num_tiles_y = (render_h + tile_size - 1) / tile_size;
+
+    auto f = vol_path_tracing;
+    if (scene.options.vol_path_version == 1) {
+        f = vol_path_tracing_1;
+    }
+    else if (scene.options.vol_path_version == 2) {
+        f = vol_path_tracing_2;
+    }
+    else if (scene.options.vol_path_version == 3) {
+        f = vol_path_tracing_3;
+    }
+    else if (scene.options.vol_path_version == 4) {
+        f = vol_path_tracing_4;
+    }
+    else if (scene.options.vol_path_version == 5) {
+        f = vol_path_tracing_5;
+    }
+    else if (scene.options.vol_path_version == 6) {
+        f = vol_path_tracing;
+    }
+
+    ProgressReporter reporter(num_tiles_x * num_tiles_y);
+    parallel_for([&](const Vector2i& tile) {
+        pcg32_state rng = init_pcg32(tile[1] * num_tiles_x + tile[0]);
+        int x0 = tile[0] * tile_size;
+        int x1 = std::min(x0 + tile_size, render_w);
+        int y0 = tile[1] * tile_size;
+        int y1 = std::min(y0 + tile_size, render_h);
+        for (int y = y0; y < y1; y++) {
+            for (int x = x0; x < x1; x++) {
+                int sample_x = x + offset_x;
+                int sample_y = y + offset_y;
+                Spectrum radiance = make_zero_spectrum();
+                int spp = scene.options.samples_per_pixel;
+                for (int s = 0; s < spp; s++) {
+                    Spectrum L = f(scene, sample_x, sample_y, rng);
+                    if (isfinite(L)) {
+                        radiance += L;
+                    }
+                }
+                img(x, y) = radiance / Real(spp);
+            }
+        }
+        reporter.update(1);
+        }, Vector2i(num_tiles_x, num_tiles_y));
+    reporter.done();
+    return img;
+}
+
+
+// main render function
+Image3 render(const Scene& scene) {
+    if (scene.options.integrator == Integrator::Depth ||
+        scene.options.integrator == Integrator::ShadingNormal ||
+        scene.options.integrator == Integrator::MeanCurvature ||
+        scene.options.integrator == Integrator::RayDifferential ||
+        scene.options.integrator == Integrator::MipmapLevel) {
+        return aux_render(scene);
+    }
+    else if (scene.options.integrator == Integrator::Path) {
+        return path_render(scene);
+    }
+    else if (scene.options.integrator == Integrator::VolPath) {
+        return vol_path_render(scene);
+    }
+    else {
+        assert(false);
+        return Image3();
+    }
+}
+
+
+#else
+
+// Default: Render Full-size Image
 /// Render auxiliary buffers e.g., depth.
-Image3 aux_render(const Scene &scene) {
+Image3 aux_render(const Scene& scene) {
     int w = scene.camera.width, h = scene.camera.height;
     Image3 img(w, h);
 
@@ -17,7 +258,7 @@ Image3 aux_render(const Scene &scene) {
     int num_tiles_x = (w + tile_size - 1) / tile_size;
     int num_tiles_y = (h + tile_size - 1) / tile_size;
 
-    parallel_for([&](const Vector2i &tile) {
+    parallel_for([&](const Vector2i& tile) {
         int x0 = tile[0] * tile_size;
         int x1 = min(x0 + tile_size, w);
         int y0 = tile[1] * tile_size;
@@ -28,47 +269,52 @@ Image3 aux_render(const Scene &scene) {
                 RayDifferential ray_diff = init_ray_differential(w, h);
                 if (std::optional<PathVertex> vertex = intersect(scene, ray, ray_diff)) {
                     Real dist = distance(vertex->position, ray.org);
-                    Vector3 color{0, 0, 0};
+                    Vector3 color{ 0, 0, 0 };
                     if (scene.options.integrator == Integrator::Depth) {
-                        color = Vector3{dist, dist, dist};
-                    } else if (scene.options.integrator == Integrator::ShadingNormal) {
+                        color = Vector3{ dist, dist, dist };
+                    }
+                    else if (scene.options.integrator == Integrator::ShadingNormal) {
                         // color = (vertex->shading_frame.n + Vector3{1, 1, 1}) / Real(2);
                         color = vertex->shading_frame.n;
-                    } else if (scene.options.integrator == Integrator::MeanCurvature) {
+                    }
+                    else if (scene.options.integrator == Integrator::MeanCurvature) {
                         Real kappa = vertex->mean_curvature;
-                        color = Vector3{kappa, kappa, kappa};
-                    } else if (scene.options.integrator == Integrator::RayDifferential) {
-                        color = Vector3{ray_diff.radius, ray_diff.spread, Real(0)};
-                    } else if (scene.options.integrator == Integrator::MipmapLevel) {
-                        const Material &mat = scene.materials[vertex->material_id];
-                        const TextureSpectrum &texture = get_texture(mat);
-                        auto *t = std::get_if<ImageTexture<Spectrum>>(&texture);
+                        color = Vector3{ kappa, kappa, kappa };
+                    }
+                    else if (scene.options.integrator == Integrator::RayDifferential) {
+                        color = Vector3{ ray_diff.radius, ray_diff.spread, Real(0) };
+                    }
+                    else if (scene.options.integrator == Integrator::MipmapLevel) {
+                        const Material& mat = scene.materials[vertex->material_id];
+                        const TextureSpectrum& texture = get_texture(mat);
+                        auto* t = std::get_if<ImageTexture<Spectrum>>(&texture);
                         if (t != nullptr) {
-                            const Mipmap3 &mipmap = get_img3(scene.texture_pool, t->texture_id);
-                            Vector2 uv{modulo(vertex->uv[0] * t->uscale, Real(1)),
-                                       modulo(vertex->uv[1] * t->vscale, Real(1))};
+                            const Mipmap3& mipmap = get_img3(scene.texture_pool, t->texture_id);
+                            Vector2 uv{ modulo(vertex->uv[0] * t->uscale, Real(1)),
+                                       modulo(vertex->uv[1] * t->vscale, Real(1)) };
                             // ray_diff.radius stores approximatedly dpdx,
                             // but we want dudx -- we get it through
                             // dpdx / dpdu
                             Real footprint = vertex->uv_screen_size;
                             Real scaled_footprint = max(get_width(mipmap), get_height(mipmap)) *
-                                                    max(t->uscale, t->vscale) * footprint;
+                                max(t->uscale, t->vscale) * footprint;
                             Real level = log2(max(scaled_footprint, Real(1e-8f)));
-                            color = Vector3{level, level, level};
+                            color = Vector3{ level, level, level };
                         }
                     }
                     img(x, y) = color;
-                } else {
-                    img(x, y) = Vector3{0, 0, 0};
+                }
+                else {
+                    img(x, y) = Vector3{ 0, 0, 0 };
                 }
             }
         }
-    }, Vector2i(num_tiles_x, num_tiles_y));
+        }, Vector2i(num_tiles_x, num_tiles_y));
 
     return img;
 }
 
-Image3 path_render(const Scene &scene) {
+Image3 path_render(const Scene& scene) {
     int w = scene.camera.width, h = scene.camera.height;
     Image3 img(w, h);
 
@@ -77,7 +323,7 @@ Image3 path_render(const Scene &scene) {
     int num_tiles_y = (h + tile_size - 1) / tile_size;
 
     ProgressReporter reporter(num_tiles_x * num_tiles_y);
-    parallel_for([&](const Vector2i &tile) {
+    parallel_for([&](const Vector2i& tile) {
         // Use a different rng stream for each thread.
         pcg32_state rng = init_pcg32(tile[1] * num_tiles_x + tile[0]);
         int x0 = tile[0] * tile_size;
@@ -95,12 +341,12 @@ Image3 path_render(const Scene &scene) {
             }
         }
         reporter.update(1);
-    }, Vector2i(num_tiles_x, num_tiles_y));
+        }, Vector2i(num_tiles_x, num_tiles_y));
     reporter.done();
     return img;
 }
 
-Image3 vol_path_render(const Scene &scene) {
+Image3 vol_path_render(const Scene& scene) {
     int w = scene.camera.width, h = scene.camera.height;
     Image3 img(w, h);
 
@@ -111,20 +357,25 @@ Image3 vol_path_render(const Scene &scene) {
     auto f = vol_path_tracing;
     if (scene.options.vol_path_version == 1) {
         f = vol_path_tracing_1;
-    } else if (scene.options.vol_path_version == 2) {
+    }
+    else if (scene.options.vol_path_version == 2) {
         f = vol_path_tracing_2;
-    } else if (scene.options.vol_path_version == 3) {
+    }
+    else if (scene.options.vol_path_version == 3) {
         f = vol_path_tracing_3;
-    } else if (scene.options.vol_path_version == 4) {
+    }
+    else if (scene.options.vol_path_version == 4) {
         f = vol_path_tracing_4;
-    } else if (scene.options.vol_path_version == 5) {
+    }
+    else if (scene.options.vol_path_version == 5) {
         f = vol_path_tracing_5;
-    } else if (scene.options.vol_path_version == 6) {
+    }
+    else if (scene.options.vol_path_version == 6) {
         f = vol_path_tracing;
     }
 
     ProgressReporter reporter(num_tiles_x * num_tiles_y);
-    parallel_for([&](const Vector2i &tile) {
+    parallel_for([&](const Vector2i& tile) {
         // Use a different rng stream for each thread.
         pcg32_state rng = init_pcg32(tile[1] * num_tiles_x + tile[0]);
         int x0 = tile[0] * tile_size;
@@ -146,25 +397,31 @@ Image3 vol_path_render(const Scene &scene) {
             }
         }
         reporter.update(1);
-    }, Vector2i(num_tiles_x, num_tiles_y));
+        }, Vector2i(num_tiles_x, num_tiles_y));
     reporter.done();
     return img;
 }
 
 
-Image3 render(const Scene &scene) {
+Image3 render(const Scene& scene) {
     if (scene.options.integrator == Integrator::Depth ||
-            scene.options.integrator == Integrator::ShadingNormal ||
-            scene.options.integrator == Integrator::MeanCurvature ||
-            scene.options.integrator == Integrator::RayDifferential ||
-            scene.options.integrator == Integrator::MipmapLevel) {
+        scene.options.integrator == Integrator::ShadingNormal ||
+        scene.options.integrator == Integrator::MeanCurvature ||
+        scene.options.integrator == Integrator::RayDifferential ||
+        scene.options.integrator == Integrator::MipmapLevel) {
         return aux_render(scene);
-    } else if (scene.options.integrator == Integrator::Path) {
+    }
+    else if (scene.options.integrator == Integrator::Path) {
         return path_render(scene);
-    } else if (scene.options.integrator == Integrator::VolPath) {
+    }
+    else if (scene.options.integrator == Integrator::VolPath) {
         return vol_path_render(scene);
-    } else {
+    }
+    else {
         assert(false);
         return Image3();
     }
 }
+
+
+#endif
